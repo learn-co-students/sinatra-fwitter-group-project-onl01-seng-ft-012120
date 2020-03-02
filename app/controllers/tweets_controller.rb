dbd03d03ac2685ce5824bce15ec41e3d067be0bf
reddevilcero <reddevil_cero@hotmail.com>
2020-03-02 20:08:20 +0000

    edit test passed

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,7 +1,11 @@
 class TweetsController < ApplicationController
 
 get '/tweets/new' do 
+  if Helpers.is_logged_in?(session)
     erb :'/tweets/new'
+  else
+    redirect 'login'
+  end
 end 
 
 post '/tweets' do 
@@ -18,9 +22,38 @@ post '/tweets' do
 end 
 
 get '/tweets/:id' do 
+  @user = Helpers.current_user(session)
+  if Helpers.is_logged_in?(session)
     @tweet = Tweet.find_by_id(params[:id])
     erb :'/tweets/show'
+  else
+    redirect '/login'
+  end
+   
 end 
+
+get '/tweets/:id/edit' do
+  @tweet = Tweet.find_by_id(params[:id])
+  if !Helpers.is_logged_in?(session)
+    redirect '/login'
+  else
+    if Helpers.current_user(session).id == @tweet.user_id
+      erb :'tweets/edit'
+    else
+      redirect "/tweets/#{@tweet.id}"
+    end
+  end
+end
+
+  patch '/tweets/:id' do
+    tweet = Tweet.find_by_id(params[:id])
+    if !Helpers.params_empty?(params)
+      tweet.update(content: params[:content])
+      redirect "tweets/#{tweet.id}"
+    else
+      redirect "/tweets/#{tweet.id}/edit"
+    end
+  end
 
 get '/tweets' do 
   if Helpers.is_logged_in?(session)
