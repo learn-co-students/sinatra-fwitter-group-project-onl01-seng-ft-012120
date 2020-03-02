2fbe323dd3e01fd5dcf11176a270a7a41189cc07
Dave Winniford <dwinniford@gmail.com>
2020-03-02 12:44:01 -0600

    adds post tweet

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,21 @@
 class TweetsController < ApplicationController
 
+get '/tweets/new' do 
+    erb :'/tweets/new'
+end 
+
+post '/tweets' do 
+    if !Helpers.params_empty?(params)
+        new_tweet = Helpers.current_user(session).tweets.build(content: params[:content])
+        new_tweet.save
+        redirect "/tweets/#{new_tweet.id}"
+    else
+        @error = "Error: content was empty"
+        erb :'/tweets/new'
+    end 
+
+end 
+
 get '/tweets' do 
   if Helpers.is_logged_in?(session)
     erb :'/tweets/index'
@@ -7,5 +23,7 @@ get '/tweets' do
     redirect '/login'
   end
 end 
+
+
 
 end
